ce1d8d05d49fcef4c707b54051b0c8329e797500
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-03-25 09:20:03 +0100

    Tag 1

---
+++ src/main/java/de/lubowiecki/Starter.java
@@ -0,0 +1,27 @@
+package de.lubowiecki;
+
+// de.lubowiecki.Starter
+
+public class Starter { // Deklaration der Klasse
+
+    // Einstiegspunkt
+    public static void main(String[] args) { // Deklaration einer Methode
+
+        // TODO: Das will ich mir noch mal anschauen
+        System.out.println("Hallo Java");
+
+        // int: Datentyp
+        // i: Name der Variable
+        // = 100: Zuweisung
+        int i = 160; // Variable
+
+        i = 200; // Änderung
+        System.out.println(i); // Gibt die Variable aus
+
+        // byte ist kleiner als int
+        //byte b = i; // Error
+
+        // double ist größer als int
+        double d = i; // Ok
+    }
+}
---
+++ src/main/java/de/lubowiecki/uebungen/Aufgabe1.java
@@ -0,0 +1,13 @@
+package de.lubowiecki.uebungen;
+
+public class Aufgabe1 {
+
+    /*
+    Zu Beginn des Programmes wird der Variable a der Wert 1 zugewiesen.
+    Direkt im Anschluss wird dieser Wert ausgegeben.
+    Daraufhin wird der Wert von a um 1 erhöht und erneut ausgegeben.
+    Erneut wird a um 1 erhöht und sein Wert anschließend ausgegeben …
+    dies wiederholt sich bis a den Wert 5 hat.
+    Dann endet das Programm.
+     */
+}
