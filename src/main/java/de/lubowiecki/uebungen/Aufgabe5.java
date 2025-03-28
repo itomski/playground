a207b63181bc08a2777e451f369d6a0a6c0191bb
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-03-28 13:30:10 +0100

    Tag 4

---
+++ src/main/java/de/lubowiecki/uebungen/Aufgabe5.java
@@ -0,0 +1,15 @@
+package de.lubowiecki.uebungen;
+
+public class Aufgabe5 {
+
+    /* Schreibe 3 Klassen, die später die Daten einer Kursverwaltung repräsentieren werden
+    Dozent, Teilnehmer, Kurs
+    Definiere in jeder Klasse, die aus deiner Sicht wichtigen Eigenschaften und verwende dafür die passenden Datentypen
+    Wende Datenkapselung an d.h. alle Eigenschaften sollen privat sein und es soll dafür Getter und Setter geben.
+    Deklariere auch passende Konstruktoren.
+
+    Instanziere jede der Klasse 3 mal und schaue dir die Daten im Debugger an.
+     */
+
+
+}
