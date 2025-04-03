83e608001c2f7ef0ca4fd2f3e2bddd75d188d039
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-04-03 13:24:28 +0200

    Tag 8

---
+++ src/main/java/de/lubowiecki/uebungen/Aufgabe9.java
@@ -0,0 +1,19 @@
+package de.lubowiecki.uebungen;
+
+public class Aufgabe9 {
+
+    /*
+    Erweiterung
+    Implementiere eine Klasse VerderblichesProdukt.
+    Diese Klasse soll von Produkt erben und es um ein Verfallsdatum erweitern.
+    Die Benutzeroberfläche soll so erweitert werden, dass der Benutzer entscheiden kann, ob er
+    ein „normales" Produkt oder ein VerderblichesProdukt erfassen möchte.
+    Die Ausgabe verderblicher Produkte soll das Verfallsdatum enthalten.
+
+    Erweiterung
+    Bei der Ausgabe soll das Datum in landesüblicher Schreibweise ausgegeben werden.
+    Siehe: https://falconbyte.net/date-time-api-in-java
+     */
+
+
+}
